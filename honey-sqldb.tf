e10c8b257fc0f1c18eb732b8f5132d8a75195a59
Matteo Potito Giorgio <zaelyuma25@gmail.com>
2024-05-31 10:16:43 +0200

    sql alert created

--- honey-sqldb.tf
+++ honey-sqldb.tf
@@ -46,14 +46,14 @@ resource "azurerm_mssql_server_extended_auditing_policy" "sqlsrv_auditing_policy
 }
 
 resource "azurerm_mssql_database_extended_auditing_policy" "sqldb_auditing_policy" {
-  database_id            = "${azurerm_mssql_server.sqlsrv_credit_cards.id}/databases/master"
+  database_id            = azurerm_mssql_database.sqldb_credit_cards.id
   log_monitoring_enabled = true
   depends_on             = [azurerm_mssql_database.sqldb_credit_cards, azurerm_mssql_server_extended_auditing_policy.sqlsrv_auditing_policy]
 }
 
 resource "azurerm_monitor_diagnostic_setting" "ds_sqldb" {
   name                           = "ds_sqldb"
-  target_resource_id             = "${azurerm_mssql_server.sqlsrv_credit_cards.id}/databases/master"
+  target_resource_id             = azurerm_mssql_database.sqldb_credit_cards.id
   log_analytics_workspace_id     = azurerm_log_analytics_workspace.monitor.id
   log_analytics_destination_type = "Dedicated"
 
@@ -66,3 +66,32 @@ resource "azurerm_monitor_diagnostic_setting" "ds_sqldb" {
 }
 
 # Alert Rule
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "alert_sql" {
+  name                = "alert_sql"
+  location            = azurerm_resource_group.rg_honey_test.location
+  resource_group_name = azurerm_resource_group.rg_honey_test.name
+
+  action {
+    action_group = [azurerm_monitor_action_group.alert_email_action.id]
+  }
+  data_source_id = azurerm_log_analytics_workspace.monitor.id
+  description    = "Alert when the SQL DB is accessed"
+  enabled        = true
+  # Count all requests with server error result code grouped into 5-minute bins
+  query       = <<-QUERY
+AzureDiagnostics
+| where client_ip_s != "Internal"
+| extend RequesterUser = iff(session_server_principal_name_s == server_principal_name_s, tostring(session_server_principal_name_s), strcat(session_server_principal_name_s, ", ", server_principal_name_s))
+| project TimeGenerated, RequesterUser, client_ip_s, ResourceGroup, LogicalServerName_s, database_name_s, action_name_s
+
+  QUERY
+  severity    = 1
+  frequency   = 5
+  time_window = 30
+  trigger {
+    operator  = "GreaterThan"
+    threshold = 0
+  }
+  depends_on = [azurerm_monitor_action_group.alert_email_action]
+}
